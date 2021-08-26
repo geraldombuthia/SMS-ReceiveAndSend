70f77ab47e30a0b1102522e14e6cc0bf6bb0fd70
Rutherford001 <geraldombuthia@gmail.com>
2021-08-26 16:06:33 +0300

    first commit

---
+++ ReceiveSms.py
@@ -0,0 +1,38 @@
+import serial
+import os, time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+port = serial.Serial('/dev/serial0', baudrate=9600, timeout=1)
+
+port.write(b'AT\r')
+rcv = port.read(10)
+print(rcv)
+time.sleep(1)
+
+port.write(b"ATE0\r")#
+rcv = port.read(10)
+print(rcv)
+time.sleep(1)
+
+port.write(b"AT+CMGF=1\r")#set sms to text mode
+rcv = port.read(10)
+print(rcv)
+time.sleep(1)
+print("Text Mode Enabled…")
+time.sleep(1)
+
+port.write(b'AT+CPMS="SM","SM","SM"\r')
+rcv = port.read(30)
+print(rcv)
+time.sleep(1)
+
+port.write(b'AT+CMGR=1\r')#Read sms message
+rcv = port.read(100)
+print(rcv)
+time.sleep(1)
+
+port.write(b'AT+CMGL="ALL"\r')
+rcv = port.read(300)
+print(rcv)
+time.sleep(1)
---
+++ sms.py
@@ -0,0 +1,34 @@
+import serial
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+
+receiverNum = '+254725737432'
+sim800l = serial.Serial(
+port='/dev/serial0',
+baudrate = 9600,
+parity=serial.PARITY_NONE,
+stopbits=serial.STOPBITS_ONE,
+bytesize=serial.EIGHTBITS,
+timeout=1
+)
+
+sms = "Hello"
+time.sleep(1)
+sim800l.write(b'AT\r')
+print(sim800l.read(24))
+time.sleep(1)
+
+sim800l.write(b'AT+CMGF=1\r')
+print(sim800l.read(24))
+time.sleep(1)
+#cmd1 = b'AT+CMGS=\' '+str(receiverNum)+'\'\r'
+sim800l.write(b'AT+CMGS="+254725737432"\r')
+print(sim800l.read(24))
+
+time.sleep(1)
+sim800l.write(str.encode(sms+chr(26)))
+#sim800l.write(chr(26))
+print(sim800l.read(24))
+
